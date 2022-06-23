85bd8f5d14fb111aefdebf3ebd88dd2a907dfb15
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-06-23 19:35:35 -0400

    no css

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -5,22 +5,22 @@ const router = require('express').Router();
 
   //creating route to html file
   router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
 
   //creating route for animals.html
   router.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
   });
 
   //creating route to zookeepers.html 
   router.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
   });
 
   //creating Wildcard route --> for links that don't exist
   router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
 
   module.exports = router;
